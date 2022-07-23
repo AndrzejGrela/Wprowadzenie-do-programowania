1e6c9c994c1deaf0abe04be61cac7808bba1122e
lenovo <andrzejgrela92@gmail.com>
2022-07-23 11:46:58 +0200

    dodałem klasę main z systememoutprintline hello world

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void  main    (String[] args){
-        System.out.println("Hello World");
+        String name = "Andrzej";
+        int wiek = 29;
+        System.out.println("Hello World "+name +"! mam " +wiek + " lat");
+        char a = '&' ;
     }
 }
