bdf4d3c73edfe6c5a6f7e1241eb9b05f31dacbb1
lenovo <andrzejgrela92@gmail.com>
2022-07-23 13:09:45 +0200

    tworzenie pętli plus kotek

---
+++ src/forLoop.java
@@ -0,0 +1,13 @@
+public class forLoop {
+    public static void main(String[] args) {
+
+        //robimy pętle
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(i);
+            if (i == 5) {
+                System.out.println("kotek");
+            }
+        }
+    }
+
+}
